81774ec1344a9339b3ad5eea47c10139ef3f6b49
sethCantCode <56614509+sethCantCode@users.noreply.github.com>
2021-04-28 14:08:03 -0400

    added components

---
+++ src/Components/landingPage/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../CSS/Home.css";
+
+function Home() {
+  return (
+    <div id="header">
+      <div className="row">
+        <div className="col-lg-6"></div>
+        <h1>Ecommerce Home</h1>
+        <div className="col-lg-6"></div>
+      </div>
+      <h1>Ecommerce Home</h1>
+    </div>
+  );
+}
+
+export default Home;
